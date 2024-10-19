163e51569de47753a7e94462d468a875620fb32e
NeilPatilBooth <npatil@ChicagoBooth.edu>
2024-10-19 16:49:20 +0000

    assemble Google Maps Query

--- solution.rb
+++ solution.rb
@@ -5,7 +5,15 @@ require "dotenv/load"
 #Hidden Variables
 google_maps_key=ENV.fetch("google_maps")
 pirate_weather_key=ENV.fetch("pirate_weather")
-pp "What city are you currently in?" 
+
+#user input
+pp "What city are you currently in? Use correct capitalization please."
+#assign user input into a variable
 user_city = gets.chomp
-pp google_maps_key
-pp pirate_weather_key
+
+#Assemble the url of Google Maps Query
+maps_request= "https://maps.googleapis.com/maps/api/geocode/json?address="+user_city+"&key="+google_maps_key
+#pp google_maps_key
+#pp pirate_weather_key
+pp user_city
+pp maps_request
