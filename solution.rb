6de5c11503c22ec55abf81a68c6b0f2b716e4483
NeilPatilBooth <npatil@ChicagoBooth.edu>
2024-10-19 16:58:36 +0000

    building the GET Request

--- solution.rb
+++ solution.rb
@@ -8,12 +8,12 @@ pirate_weather_key=ENV.fetch("pirate_weather")
 
 #user input
 pp "What city are you currently in? Use correct capitalization please."
-#assign user input into a variable
+#assign user city input into a variable
 user_city = gets.chomp
 
 #Assemble the url of Google Maps Query
-maps_request= "https://maps.googleapis.com/maps/api/geocode/json?address="+user_city+"&key="+google_maps_key
+maps_request_url= "https://maps.googleapis.com/maps/api/geocode/json?address="+user_city+"&key="+google_maps_key
+#Place a GET Request 
+raw_maps_url=HTTP.get(maps_request_url)
 #pp google_maps_key
 #pp pirate_weather_key
-pp user_city
-pp maps_request
