1ba15e0bd1fb81626bddfa6094c6e48c711888da
NeilPatilBooth <npatil@ChicagoBooth.edu>
2024-10-19 15:55:10 +0000

    added keys

--- solution.rb
+++ solution.rb
@@ -2,5 +2,10 @@ require "http"
 require "json"
 require "dotenv/load"
 
+#Hidden Variables
+google_maps_key=ENV.fetch("google_maps")
+pirate_weather_key=ENV.fetch("pirate_weather")
 pp "What city are you currently in?" 
 user_city = gets.chomp
+pp google_maps_key
+pp pirate_weather_key
